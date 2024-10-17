3de1d0b906c4edd4d4f5f740a0ff3dcb373537f7
Tip19 <max.andersson4@elev.ga.ntig.se>
2024-10-17 14:25:04 +0200

    computer management update

--- Programering 1/computer_management.py
+++ Programering 1/computer_management.py
@@ -14,19 +14,39 @@ computer_list = [
     "backup dator",
 ]
 service_list = []
+        
+
+def change_service_choice():
+    service_choice = input (f"vill du ta bort en dator från service list? ")
+    if service_choice == "ja":
+        service_choice = input (f"vilken dator vill du ta bort {service_list} ")
+        if service_choice in service_list:
+            service_list.remove(service_choice)
+            computer_list.append(service_choice)
+            pc_serviced -=1
+        else:
+            service_choice = input (f"denna dator existerar inte vilken dator vill du ta bort {service_list} ")
+        return computer_list, service_choice, service_list
+
 
 pc_serviced = 0
-while pc_serviced < 3:
+
+while pc_serviced < 2:
     service_choice = input (f"vilken dator ska få service? {computer_list} ")
-    time.sleep(1)
-    service_list.append(service_choice)
-    computer_list.remove(service_choice)
+    if service_choice in service_list or service_choice in computer_list:
+        time.sleep(1)
+        service_list.append(service_choice)
+        computer_list.remove(service_choice)
+        pc_serviced +=1
+    else:
+        service_choice = input (f"denn dator existerar inte vilken dator ska få service? {computer_list} ")
     print (f"\n\ncomputers in the service list: {service_list}\n")
     time.sleep(3)
     print (f"all computers: {computer_list}\n\n")
     time.sleep(3)
-    pc_serviced+=1
-    if pc_serviced > 1:
-        service_choice = input (f"vill du ta bort en dator från service list")
+
+    if pc_serviced >= 1:
+        change_service_choice()
+        time.sleep(1)
 
 
--- Python projekt/Coding_bat/first_last.py
+++ Python projekt/Coding_bat/first_last.py
@@ -1,4 +1,4 @@
-start = input ("Input at least 1 or more numbers")
+start = int(input ("Input at least 1 or more numbers"))
 while start <= 0:
-    start = input ("Input at least 1 or more numbers")
+    start = int(input ("Input at least 1 or more numbers"))
 
