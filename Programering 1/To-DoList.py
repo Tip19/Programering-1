6f0d6cca4d79fd400f3013d185dd6b8bd08250dd
Tip19 <max.andersson4@elev.ga.ntig.se>
2025-01-15 14:45:23 +0100

    To-DoList update definitions

--- Programering 1/To-DoList.py
+++ Programering 1/To-DoList.py
@@ -3,24 +3,28 @@ To_Do_list = [ # stores the list of tasks
     {"task": "homework"},
     {"task": "walk the dog"},
 ]
+
 #lets user add something to their list
-def Edit_list(Add_remove):
+def Add(Add_remove):
     if Add_Remove == "add": # runs code below only if the user wants to add something to the To-Do list
         time.sleep(1)
         Add = input ("what do you want to add to your To-Do list?\nAnswer: ").lower()
         To_Do_list.append({"task": Add}) # Adds the item to their To-Do list (Help with syntax by AI)
         return Add
     
-# lets user type what they want to remove and checks if it is in the list
-    elif Add_Remove == "remove":
+# lets user remove something from their list
+def Remove(Add_remove):
+    if Add_Remove == "remove":
         time.sleep(2)
-        Remove = input ("What do you want to remove from the list?\nAnswer: ").lower()
-        if {"task": Remove} in To_Do_list: # Checks if the item the user wants to remove is in the list
-            To_Do_list.remove({"task": Remove}) # (Help with syntax by AI)
-            return Remove
-        else:
-            time.sleep(2)
-            print ("\nthis task is not in your To-Do list")
+        print ("What do you want to remove from the list?")
+        time.sleep(1)
+        Remove = input ("\nAnswer: ").lower()
+    if {"task": Remove} in To_Do_list: # Checks if the item the user wants to remove is in the list
+        To_Do_list.remove({"task": Remove}) # (Help with syntax by AI)
+        return Remove
+    else:
+        time.sleep(2)
+        print ("\nthis task is not in your To-Do list")
 
 # prints the list and asks if you want to add or remove something
 while True: # loops until the user is either done adding or removing from their list
@@ -28,7 +32,10 @@ while True: # loops until the user is either done adding or removing from their 
      time.sleep(1)
      print(f"To-Do list: {item['task']}\n") # prints each item in the list(Help with syntax by AI)
      time.sleep(1)
-    Add_Remove = input ("do you want to add or remove something from your To-Do list?\nAnswer: ").lower()
-    if Add_Remove == "no":
+    Add_Remove = input ("do you want to add or remove something from your To-Do list or are you done?\nAnswer: ").lower()
+    if Add_Remove == "no" or "done":
         break
-    Edit_list(Add_Remove) # runs the code to remove or add things check line 9 - 24
+    elif Add_Remove == "add":
+        Add(Add_Remove)
+    elif Add_Remove == "remove":
+        Remove(Add_Remove)
